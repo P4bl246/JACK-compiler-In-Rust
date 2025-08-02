9fccea360b97e19959b9c3255417a826a49df501
P4bl246 <cr1pt0c4b4nd0@email.com>
2025-08-02 16:44:25 -0500

    d

---
+++ SourceCode/Code_fileForMakeConfig/prepareFile.rs
@@ -0,0 +1,15 @@
+pub mod removeComments{
+   
+   pub fn removeSimpleComments(file_Input:){
+    // Function to remove single-line comments from the input file
+    // Función para eliminar comentarios de una línea del archivo de entrada
+    
+        
+   }
+//-------------------------------------------------
+    pub fn removeBlockComments(file_Input:){
+     // Function to remove multi-line or 'block' comments from the input file
+     // Función para eliminar comentarios de varias líneas o en 'bloque' del archivo de entrada
+    }
+
+}
